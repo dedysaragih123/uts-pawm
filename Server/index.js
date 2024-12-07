12d98b267374c4d3be00ff5bcc09a830a23ec197
dedysaragih123 <dedyhofmanindosaragih@gmail.com>
2024-12-07 13:22:55 +0700

    ...

--- Server/index.js
+++ Server/index.js
@@ -67,7 +67,7 @@ app.use(session({
         secure: process.env.NODE_ENV === 'production', // Enable secure cookies in production
         httpOnly: true,
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-        sameSite: 'lax',
+        sameSite: 'None',
     },
 }));
 
